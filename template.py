1e155740753e9f56694632b1233f707b9e2beb2f
Sudheesh E <sudheeshedathala@gmail.com>
2022-05-14 23:51:37 +0530

    first commit

---
+++ template.py
@@ -0,0 +1,37 @@
+import os
+
+
+directories = [
+
+    os.path.join('data', 'raw'),
+    os.path.join('data','processed'),
+    'data_given',
+    'notebooks',
+    'saved_models',
+    'src'
+]
+
+try:
+    for dir_ in directories:
+        os.makedirs(dir_, exist_ok=True)
+        with open(os.path.join(dir_, '.gitkeep'), 'w') as f:
+            pass
+
+except Exception as e:
+    print(e)
+
+
+file_ = [
+    'dvc.yaml',
+    'params.yaml',
+    '.gitignore',
+    os.path.join('src', '__init__.py')
+]
+
+try:
+    for file in file_:
+        with open(file, 'w') as f:
+            pass
+
+except Exception as e:
+    print(e)
